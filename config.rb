b50d87339c8892b55b8240290602c8a3d90d19ee
Bruce Woodward <bruce.woodward@gmail.com>
2013-12-31 13:06:18 +1100

    Stealing config.rb
    Taking the config.rb from Carl that he stole from someone else.

--- config.rb
+++ config.rb
@@ -1,70 +1,41 @@
-###
-# Compass
-###
+require "redcarpet"
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+Time.zone = "Australia/Sydney"
 
-###
-# Page options, layouts, aliases and proxies
-###
+activate :syntax
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+activate :blog do |blog|
+  blog.permalink = "/:title"
+  blog.sources = "/posts/:year-:month-:day-:title.html"
+  blog.paginate = true
+end
 
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+activate :directory_indexes
 
-###
-# Helpers
-###
+after_configuration do
+  sprockets.append_path Modernizr.path
+end
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+activate :relative_assets
+set :relative_links, true
 
-# Reload the browser automatically whenever files change
-# activate :livereload
+activate :directory_indexes
+set :trailing_slash, false
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+page "/feed.xml", layout: false
+page "/sitemap.xml", layout: false
+page "/index.html", layout: :application
+page "/posts/*", layout: :post
+
+set :haml, format: :html5
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true, smartypants: true, footnotes: true, autolink: true
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
-# Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :minify_css
+  activate :minify_js
 end
